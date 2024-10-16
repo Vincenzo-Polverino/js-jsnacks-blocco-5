d19e82fb8494e03f6c42262ecc5ab1478773ea49
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-16 18:19:26 +0200

    snack 7 complete

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,27 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+
+// con find
+
+const student = students.find(student => student.id === 2);
+
+console.log(student);
+
+
+// con ciclo for
+
+let student_for;
+
+for (let i = 0; i < students.length; i++) {
+
+  if (students[i].id === 2) {
+
+    student_for = students[i];
+
+    break;
+  }
+}
+
+console.log(student_for); 
