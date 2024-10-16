17bfd0a9dc1d66963cba39ba717584f0f7671e0f
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-16 17:15:40 +0200

    snack 1 complete

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,14 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// senza ciclo for
+names.forEach(name => {
+    console.log(name);
+});
+
+
+// con ciclo for
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
