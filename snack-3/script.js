620152b535399984415ddcd7115744d84bf439a4
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-16 17:39:47 +0200

    snack 3 complete

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,18 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// con ForEach
+numbers.forEach((number,i, numbers) => {
+    numbers[i] = number + 1;
+});
+
+console.log(numbers);
+
+
+// con ciclo for
+
+/*for (let i = 0; i < numbers.length; i++) {
+    numbers[i] +=1 ;  
+}
+console.log(numbers);*/
