67f2545d5f2323529ffaddd67598a866a3f6a997
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-10-16 17:52:28 +0200

    snack 4 complete

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,19 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+
+// con .map
+let authors = posts.map(post => post.author)
+console.log(authors);
+
+// con ciclo for
+
+let authors_for=[]
+for (let i = 0; i <posts.length; i++) {
+  const post = posts[i].author;
+  authors_for.push(post)
+
+  
+}
+console.log(authors_for);
